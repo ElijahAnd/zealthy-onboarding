1681d706e4ba49311f4c3caa59c419456c11d77f
ElijahAnd <elijahan.q@gmail.com>
2024-12-21 23:03:59 -0500

    fixed cors issue added ngrok

--- zealthy_nextjs/next.config.ts
+++ zealthy_nextjs/next.config.ts
@@ -6,10 +6,6 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/data',
-        destination: '/data/index.html',
-      },
-      {
         source: '/api/:path*',
         destination: 'http://localhost:8000/api/:path*',
       },
